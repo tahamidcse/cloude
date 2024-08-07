e398fabea96803bcd82b3f475e7d48e98e0ed1e2
Sabbir-2110676154 <tahamidcse@gmail.com>
2024-08-07 16:32:36 +0600

    Initial commit

---
+++ server/envi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\st\downloads\cloud\cloud\server\envi\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ server/user/main.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main() {int x;cin>>x;cout<<x<<endl;return 0;}
